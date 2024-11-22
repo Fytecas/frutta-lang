d07815d9719b41c43519647a034a4fd8959265e8
Dario Le Hy <98255583+Fytecas@users.noreply.github.com>
2024-11-22 10:14:26 +0000

    remove target

--- parser/target/debug/deps/parser-f9cc5627a8a9576b.d
+++
@@ -1,8 +0,0 @@
-/home/dario/Documents/dario/official/frutta/parser/target/debug/deps/parser-f9cc5627a8a9576b: src/lib.rs src/errors.rs src/tokens.rs src/expr.rs
-
-/home/dario/Documents/dario/official/frutta/parser/target/debug/deps/parser-f9cc5627a8a9576b.d: src/lib.rs src/errors.rs src/tokens.rs src/expr.rs
-
-src/lib.rs:
-src/errors.rs:
-src/tokens.rs:
-src/expr.rs:
